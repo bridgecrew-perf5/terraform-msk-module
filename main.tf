9ffef695fc1c9cbbb7c6f08eb73c4fdd635719ea
Antonio Rufo <kitt_cuenco@yahoo.com>
2021-09-24 11:10:08 +1000

    Interpolated server_properties

--- main.tf
+++ main.tf
@@ -72,19 +72,6 @@ resource "aws_msk_configuration" "SGDEFAULTMSK" {
   kafka_versions = var.msk_config_kafka_versions
   name           = var.msk_config_name
 
-  server_properties = <<PROPERTIES
-  auto.create.topics.enable=true
-  default.replication.factor=2
-  min.insync.replicas=2
-  num.io.threads=8
-  num.network.threads=5
-  num.partitions=2
-  num.replica.fetchers=2
-  replica.lag.time.max.ms=30000
-  socket.receive.buffer.bytes=102400
-  socket.request.max.bytes=104857600
-  socket.send.buffer.bytes=102400
-  unclean.leader.election.enable=true
-  zookeeper.session.timeout.ms=18000
-PROPERTIES
+  server_properties = var.server_properties
+
 }
